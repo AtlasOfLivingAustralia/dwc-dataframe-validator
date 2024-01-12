8102dd99eded69391a7b088aae299dff3539c3c7
acbuyan <amanda.buyan@gmail.com>
2024-01-12 13:45:01 +1100

    fix(simplified-taxonomy-function-so-it-returns-closest-match-and-higher-taxonomy): simplified taxonomy function so it returns closest match and higher taxonomy

--- dwc_validator/validate.py
+++ dwc_validator/validate.py
@@ -483,7 +483,6 @@ def validate_numeric_fields(dataframe: DataFrame, warnings: List[str]):
 def create_taxonomy_report(dataframe: DataFrame,
                            num_matches: int = 5,
                            include_synonyms: bool = True,
-                           change_names_to_backbone: bool = True
                            ) -> TaxonReport:
     """
     Check if the given taxon in a data frame are valid for chosen atlas backbone.
@@ -496,8 +495,6 @@ def create_taxonomy_report(dataframe: DataFrame,
             the maximum number of possible matches to return when searching for matches in chosen atlas
         include_synonyms : logical
             an option to include any synonyms of the identified taxon in your search
-        change_names_to_backbone: logical
-            not sure if I need this yet
 
     Returns
     -------
@@ -519,72 +516,51 @@ def create_taxonomy_report(dataframe: DataFrame,
     payload = [{"scientificName": name} for name in scientific_names_list]
     response = requests.request("POST","https://api.ala.org.au/namematching/api/searchAllByClassification",data=json.dumps(payload))
     response_json = response.json()
-    verification_list = {"scientificName": scientific_names_list, "issues": [None for i in range(len(scientific_names_list))]}
-    taxonomy = {name: [None for i in range(len(scientific_names_list))] for name in taxon_terms["Australia"]} # REMOVE THIS LATER
+    invalid_taxon_dict = {x: [] for x in taxon_terms["Australia"]}
+    invalid_taxon_dict["original name"] = []
+    invalid_taxon_dict["proposed match(es)"] = []
+    invalid_taxon_dict["rank of proposed match(es)"] = []
     
     # loop over list of names and ensure we have gotten all the issues - might need to do single name search
     # to ensure we get everything
     for i,item in enumerate(scientific_names_list):
         item_index = next((index for (index, d) in enumerate(response_json) if "scientificName" in d and d["scientificName"] == item), None)
-        taxonomy["scientificName"][i] = item
-        if item_index is not None:
-            verification_list["issues"][i] = response_json[item_index]["issues"]
-        else:
-            response_single = requests.get("https://api.ala.org.au/namematching/api/search?q={}".format("%20".join(item.split(" "))))
+        # taxonomy["scientificName"][i] = item
+        if item_index is None:
+            # make this better
+            has_invalid_taxa = True
+            response_single = requests.get("https://api.ala.org.au/namematching/api/autocomplete?q={}&max={}&includeSynonyms={}".format("%20".join(item.split(" ")),5,str(True).lower()))
             response_json_single = response_single.json()
-            if response_json_single['success']:
-                if response_json_single['scientificName'] == item:
-                    verification_list["issues"][i] = response_json_single["issues"]
-                else:
-                    verification_list["issues"][i] = "homonym"
-            else:
-                verification_list["issues"][i] = response_json_single["issues"]
-
-    # check for homonyms - if there are any, then print them out to the user so the user can disambiguate the names
-    df_verification = pd.DataFrame(verification_list)
-    invalid_taxon = df_verification.loc[df_verification['issues'].astype(str).str.contains("homonym",case=False,na=False)]
-    invalid_taxon_dict = {}
-    matches = {}
-    if not invalid_taxon.empty:
-        has_invalid_taxa=True
-        matches = {x: [] for x in invalid_taxon['scientificName']}
-        for name in invalid_taxon['scientificName']:
-            response = requests.get("https://api.ala.org.au/namematching/api/autocomplete?q={}&max={}&includeSynonyms={}".format("%20".join(name.split(" ")),num_matches,str(include_synonyms).lower()))
-            response_json = response.json()
-            list_names = []
-            if response_json:
-                for match in response_json:
-                    if "synonymMatch" in match:
-                        for entry in match["synonymMatch"]:
-                            if entry['name'] == name:
-                                df_verification.loc[df_verification.scientificName == name, 'issues'] = 'noIssues'
-                                break
-                    else:
-                        ### TODO: other errors??
-                        if response_json[0]['name'] == name:
-                            df_verification.loc[df_verification.scientificName == name, 'issues'] = 'noIssues'
+            if response_json_single:
+                if response_json_single[0]['rank'] is not None:
+                    invalid_taxon_dict["original name"].append(item)
+                    invalid_taxon_dict["proposed match(es)"].append(response_json_single[0]['name'])
+                    invalid_taxon_dict["rank of proposed match(es)"].append(response_json_single[0]['rank'])
+                    for term in taxon_terms["Australia"]:
+                        if term in response_json_single[0]['cl']:
+                            invalid_taxon_dict[term].append(response_json_single[0]['cl'][term])
                         else:
-                            if name not in invalid_taxon_dict.keys():
-                                invalid_taxon_dict[name] = [match['name']]
-                            else:
-                                invalid_taxon_dict[name].append(match['name'])
-            else:
-                # perform one more check
-                response_single = requests.get("https://api.ala.org.au/namematching/api/searchByClassification?scientificName={}&rank=species".format("%20".join(name.split(" "))))
-                # response_single = requests.get("https://api.ala.org.au/namematching/api/search?q={}&limit=5".format("%20".join(name.split(" "))))
-                response_json_single = response_single.json()
-                if response_json_single['success']:
-                    if response_json_single["scientificName"] == name:
-                        df_verification.loc[df_verification.scientificName == name, 'issues'] = 'noIssues'
-                    else:
-                        invalid_taxon_dict[name] = response_json_single["scientificName"]
+                            invalid_taxon_dict[term].append(None)
                 else:
-                    print("probably a homonym")
-                    print(response_json_single)
-                    import sys
-                    sys.exit()
-            matches[name] = list_names
 
+                    # check for synonyms
+                    for synonym in response_json_single[0]["synonymMatch"]:
+                        if synonym['rank'] is not None:
+                            invalid_taxon_dict["original name"].append(item)
+                            invalid_taxon_dict["proposed match(es)"].append(synonym['name'])
+                            invalid_taxon_dict["rank of proposed match(es)"].append(synonym['rank'])
+                            for term in taxon_terms["Australia"]:
+                                if term in synonym['cl']:
+                                    invalid_taxon_dict[term].append(synonym['cl'][term])
+                            else:
+                                invalid_taxon_dict[term].append(None)
+            else:
+                print("There is a case we haven't considered yet")
+                print()
+                import sys
+                sys.exit()
+    
+    # return report on taxon
     return TaxonReport(
         has_invalid_taxa = has_invalid_taxa,
         unrecognised_taxa = invalid_taxon_dict
